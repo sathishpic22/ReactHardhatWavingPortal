9beec48aadcdd70a248dbaf224dcf29dbdbfc42e
sathishpic22 <58845085+sathishpic22@users.noreply.github.com>
2022-10-08 16:07:13 +0530

    New Wave Commit

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,8 +4,8 @@ require("@nomicfoundation/hardhat-toolbox");
 module.exports = {
   solidity: "0.8.17",
   networks: {
-    rinkeby: {
-      url: `https://rinkeby.infura.io/v3/a392979a76ba43d5bc15dc73a3e5eb4b`,
+    Goerli: {
+      url: `https://goerli.infura.io/v3/dc77b0e951614cd0873921a68ada111b`,
       accounts: ['0d6aa040ab5fd085c9b209aa4cc0734d653bb3d44e273b647c4c886b02537b04']
     }
   }
--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import './App.css';
 import lockAbi from './lockAbi';
 import React, { useEffect, useState } from "react";
 import { ethers } from "ethers";
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   const [currentAccount, setCurrentAccount] = useState("");
   const [messgae, setMessage] = useState("");
   const [count, setCount] = useState("");
-const contractaddress="0xa2B5A3C7fF298169bd8d2a97c9A414bC86814662";
+const contractaddress="0x0410f1fE4482798e5899fD9A6C60b538D0777494";
   const checkIfWalletIsConnected = async () => {
     try {
       const { ethereum } = window;
@@ -80,13 +81,32 @@ async function newwave(){
 
     if (ethereum) {
       const provider = new ethers.providers.Web3Provider(ethereum);
+      
+      const account1 = '0xc0BB46e0B7de8380cdF670186d2cFD2E9056b426' // Your account address 1
+      
+      const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]);
+      
+      const privateKey1 = '0d6aa040ab5fd085c9b209aa4cc0734d653bb3d44e273b647c4c886b02537b04' // Private key of account 1
+      const wallet = new ethers.Wallet(privateKey1, provider)
+      
       const signer = provider.getSigner();
       const wavePortalContract = new ethers.Contract(contractaddress, lockAbi.abi, signer);
       const count=await wavePortalContract.newWave("My wave ");
        console.log("New Wave is mining......");
        console.log(count); 
-
-    } else {
+      
+          const tx = await wallet.sendTransaction({
+              to: currentAccount,
+              value: ethers.utils.parseEther("0.0000050")
+          })
+      
+          await tx.wait()
+          console.log(tx)
+      
+   } else {
       console.log("Ethereum object doesn't exist!");
     }
   } catch (error) {
@@ -101,6 +121,7 @@ async function newwave(){
 
   return (
     <div className="App">
+
 <button className="waveButton" onClick={wave}>
     WaveCount
 </button>
